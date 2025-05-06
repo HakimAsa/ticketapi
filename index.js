1729a9e03c93c3ebed433f20e73a32724604c007
aamidou <akimayena26@gmail.com>
2025-05-06 14:52:43 +0100

    setting up db remotely

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const client = require('./src/startup/db')
+
+const app = express()
+app.use(express.json())
+const port = process.env.PORT || 3025
+const env = process.env.NODE_ENV || 'development'
+
+app.get('/users', async (req, res) => {
+  const result = await client.query('SELECT * FROM users')
+  res.json(result.rows)
+})
+
+app.listen(3025, () => {
+  console.log(
+    `Example app listening on port ${port} in ${env} mode...`.yellow.underline
+      .bold
+  )
+})
--- src/startup/db.js
+++ src/startup/db.js
@@ -1,18 +1,22 @@
+require('colors')
 const { Client } = require('pg')
+const config = require('config')
 
 // Create a new instance of the Client
+const db = config.get('db')
 const client = new Client({
-  host: 'localhost', // PostgreSQL server host
-  port: 5432, // Default PostgreSQL port
-  user: 'your-username', // Your PostgreSQL username
-  password: 'your-password', // Your PostgreSQL password
-  database: 'your-database-name', // Your database name
+  connectionString: db,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
 
 // Connect to PostgreSQL
 client
   .connect()
-  .then(() => console.log('Connected to PostgreSQL'))
-  .catch((err) => console.error('Connection error', err.stack))
+  .then(() =>
+    console.info(`connected to PostgeSQL at ${db}...`.cyan.underline.bold)
+  )
+  .catch((err) => console.log(err.message.red.underline.bold))
 
 module.exports = client
