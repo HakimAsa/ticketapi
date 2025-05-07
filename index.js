df9b5b57133ec8e06a01b14cb0465c273aafc3f0
aamidou <akimayena26@gmail.com>
2025-05-07 06:16:44 +0100

    secure headers

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const cors = require('cors')
+const helmet = require('helmet')
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const rateLimit = require('express-rate-limit')
 
 const client = require('./src/startup/db')
 const events = require('./src/routes/events')
@@ -16,6 +18,44 @@ const env = process.env.NODE_ENV || 'development'
 app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
+// secure headers
+
+app.use(
+  helmet({
+    crossOriginEmbedderPolicy: false,
+    contentSecurityPolicy: {
+      //CSP issue on production
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: [
+          "'self'",
+          // Add the hash from my browser error: need to be updated
+          "'sha256-ZomnyosL2bmZ79LmErHEhL+1fVaBj9NngvpOK/l4qio='",
+        ],
+        styleSrc: ["'self'", "'unsafe-inline'"], // Optional, needed if styles are inline too
+        objectSrc: ["'none'"],
+        imgSrc: ["'self'", 'data:'], // Allow images if needed
+      },
+    },
+  })
+)
+
+// Rate Limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, //10 min
+  max: 25, // 25 requests can be made in 10 min
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers,
+  message: 'Too many requests, please try again later.',
+})
+
+app.use(limiter)
+
+// Enable trust proxy
+app.set('trust proxy', 'loopback, linklocal, uniquelocal')
+app.get('/ip', (request, response) => response.send(request.ip))
+app.get('/x-forwarded-for', (request, response) =>
+  response.send(request.headers['x-forwarded-for'])
+)
 
 app.use('/api/v1/auth/admin', auth)
 app.use('/api/v1', events)
